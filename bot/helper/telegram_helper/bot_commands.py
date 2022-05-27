17529b3ed300534aff5ec9d0b09550acb39b11e2
sdhtele <97067334+sdhtele@users.noreply.github.com>
2022-05-27 17:28:33 +0700

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -33,6 +33,7 @@ class _BotCommands:
         self.DeleteCommand = f'del{CMD_INDEX}'
         self.ShellCommand = f'shell{CMD_INDEX}'
         self.ExecHelpCommand = f'exechelp{CMD_INDEX}'
+        self.EvalCommand = f'eval{CMD_INDEX}'
         self.LeechSetCommand = f'leechset{CMD_INDEX}'
         self.SetThumbCommand = f'setthumb{CMD_INDEX}'
         self.LeechCommand = f'le1{CMD_INDEX}'
